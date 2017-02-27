29383db1b15dcd61969a53faeba6374137dafc6b
Benjamin M. Mitchell <bmitchell@complete-geek.com>
2017-02-26 21:27:38 -0500

    update to remotetest.ps1

--- scripts/remotetest.ps1
+++ scripts/remotetest.ps1
@@ -1,5 +1,5 @@
 #This file is for testing remote calls via envoke-webRequest
-
+# Call w/ '(iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/BenMitchell1979/azure-minecraft/master/scripts/remotetest.ps1')))>$null 2>&1'
 mkdir $env:USERPROFILE\temp\
 New-Item $env:USERPROFILE\temp\test.txt
 
